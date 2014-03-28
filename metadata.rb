e1992d8085c56e9987043d645a066edd967932ae
Chris Doughty <cdoughty77@gmail.com>
2014-03-27 21:01:36 -0500

    Adding metadata file to fix chef vomit warnings

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Chris Doughty"
+maintainer_email "cdoughty77@gmail.com"
+license          "MIT"
+description      "Configured network interfaces file"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
