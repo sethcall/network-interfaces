3bf503dfe10dd821a0661c22684dd8b2f4641d5e
Chris Doughty <cdoughty77@gmail.com>
2014-03-27 21:04:20 -0500

    Fixing service restart notifications

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,8 @@
 template "/etc/network/interfaces" do
   source "interfaces.erb"
-  # Need to find out how to dynamically add IP Address for each host
-  variables( :INTERFACE => "eth0", :IP => "static", :ADDR => ip, :NETMASK => "255.255.255.0", :GATEWAY => "192.168.1.1")
+  notifies :restart, "service[neworking]"
 end
 
 service "networking" do
-  action [:restart]
+  action [:nothing]
 end
