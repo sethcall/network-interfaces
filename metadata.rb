95eccb4fa222cb6229f25d3e26475f2347b077dd
Seth Call <sethcall@gmail.com>
2014-12-19 13:54:25 -0600

    * adding name metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,5 @@ license          "MIT"
 description      "Configured network interfaces file"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+name		 "network-interfaces"
 
